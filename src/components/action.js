838abaf80869e947dfab7d9c2f96504075055628
Mel Escolano <amalia.escolano@gmail.com>
2022-02-09 19:21:03 +0100

    Create component files

---
+++ src/components/action.js
@@ -0,0 +1,13 @@
+export function Action() {
+  return (
+    <div className="actions">
+      <span className="number">667359961</span>
+      <a href="#" className="call">
+        Call
+      </a>
+      <a href="#" className="hang active">
+        Hang
+      </a>
+    </div>
+  );
+}
